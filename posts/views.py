52a3f83397d139f38cb0315192e5528bcf716e5e
etah <arrahetah23@gmail.com>
2024-08-16 20:14:22 +0100

    innitial code'

---
+++ posts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Posts
+
+
+# Create your views here.
+
+def posts_lists(request):
+    posts = Posts.objects.all().order_by('-date')
+    return render(request,'posts/posts_list.html',{'posts':posts})
+
+def post_page(request,slug):
+    post = Posts.objects.get(slug=slug)
+    return render(request,'posts/post_page.html',{'post':post})
